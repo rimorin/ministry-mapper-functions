d25dfcfccd21ab251cf5ba6f6970f83581aa1943
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2024-05-11 14:19:57 +0800

    node 20 migration and extra logging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,8 @@ import { EmailParams, MailerSend, Recipient, Sender } from "mailersend";
 const sgRegion = region(process.env.LOCAL_RTDB || FB_DEFAULT_CLOUD_REGION);
 
 const cleanUpLinks = (app: App, time: number) => {
+  console.log("cleanUpLinks called with time:", time);
+
   const database = getDatabase(app);
   const dbname = database.app.name;
   console.info(`Running ${dbname} link cleanup job.`);
@@ -20,9 +22,11 @@ const cleanUpLinks = (app: App, time: number) => {
     .ref("links")
     .once("value")
     .then((snapshot) => {
+      console.log("Received snapshot from database");
       snapshot.forEach((linkSnapshot) => {
         const congregation = linkSnapshot.key;
         const congregationLinkSnapshot = linkSnapshot.val();
+        console.log(`Processing congregation: ${congregation}`);
         for (const id in congregationLinkSnapshot) {
           const link = congregationLinkSnapshot[id];
           const timestamp = link.tokenEndtime;
@@ -32,6 +36,7 @@ const cleanUpLinks = (app: App, time: number) => {
             `Checking ${dbname} link id: ${congregation}, postalcode: ${postalcode}, timestamp: ${timestamp}`
           );
           if (time > timestamp) {
+            console.log(`Link ${id} is expired. Removing...`);
             database
               .ref(`links/${congregation}/${id}`)
               .remove()
@@ -43,6 +48,8 @@ const cleanUpLinks = (app: App, time: number) => {
                   `Error when removing ${dbname} link id : ${id}. Reason: ${reason}`
                 );
               });
+          } else {
+            console.log(`Link ${id} is not expired. Skipping...`);
           }
         }
       });
@@ -52,6 +59,7 @@ const cleanUpLinks = (app: App, time: number) => {
     })
     .finally(() => {
       console.info(`Completed ${dbname} link cleanup job.`);
+      console.log("Deleting app...");
       deleteApp(app);
     });
 };
@@ -124,59 +132,112 @@ const getCongUsers = async (
 
 export const getCongregationUsers = sgRegion.https.onCall(
   async (data, context) => {
+    console.log("getCongregationUsers called with data:", data);
+
     let congregationUsers: any = {};
     const congregation = data.congregation;
-    if (!congregation) return;
+    if (!congregation) {
+      console.log("No congregation provided. Exiting getCongregationUsers...");
+      return;
+    }
 
+    console.log(`Initializing app for congregation: ${congregation}`);
     const app = initializeApp();
     try {
+      console.log("Calling getCongUsers...");
       congregationUsers = await getCongUsers(app, congregation);
+      console.log(
+        "getCongUsers returned:",
+        Object.keys(congregationUsers).length
+      );
+    } catch (error) {
+      console.error("Error in getCongUsers:", error);
     } finally {
+      console.log("Deleting app...");
       deleteApp(app);
     }
+    console.log(
+      "Returning from getCongregationUsers with:",
+      Object.keys(congregationUsers).length
+    );
     return congregationUsers;
   }
 );
 
 export const getUserByEmail = sgRegion.https.onCall(async (data, context) => {
+  console.log("getUserByEmail called with data:", data);
+
   let user: UserRecord;
   const email = data.email;
-  if (!email) return;
+  if (!email) {
+    console.log("No email provided. Exiting getUserByEmail...");
+    return;
+  }
 
+  console.log(`Initializing app for email: ${email}`);
   const app = initializeApp();
   try {
+    console.log("Getting auth...");
     const auth = getAuth(app);
+    console.log("Getting user by email...");
     user = await auth.getUserByEmail(email);
+    if (!user) {
+      console.log("No user found. Exiting getUserByEmail...");
+      return;
+    }
+    console.log("User found:", user);
   } finally {
+    console.log("Deleting app...");
     deleteApp(app);
   }
+  console.log("Returning from getUserByEmail with:", user);
   return user;
 });
 
 export const updateUserAccess = sgRegion.https.onCall(async (data, context) => {
+  console.log("updateUserAccess called with data:", data);
+
   const userId = data.uid;
   const congregation = data.congregation;
   const role = data.role;
-  if (!congregation || !userId || !role) return;
+  if (!congregation || !userId || !role) {
+    console.log("Missing required data. Exiting updateUserAccess...");
+    return;
+  }
 
+  console.log(
+    `Initializing app for user: ${userId} and congregation: ${congregation}`
+  );
   const app = initializeApp();
   try {
+    console.log("Getting auth...");
     const auth = getAuth(app);
+    console.log("Getting user...");
     const user = await auth.getUser(userId);
-    if (!user) return;
+    if (!user) {
+      console.log("No user found. Exiting updateUserAccess...");
+      return;
+    }
+    console.log("User found:", user);
     const currentClaims = user.customClaims || {};
     const roleValue = Number(role);
+    console.log(`Processing role: ${roleValue}`);
     switch (roleValue) {
       case ROLE_REMOVE_ACCESS:
+        console.log("Removing access...");
         delete currentClaims[congregation];
         break;
       default:
+        console.log("Setting role...");
         currentClaims[congregation] = roleValue;
     }
+    console.log("Setting custom user claims...");
     await auth.setCustomUserClaims(userId, currentClaims);
   } finally {
+    console.log("Deleting app...");
     deleteApp(app);
   }
+  console.log("Finished updateUserAccess");
 });
 
 export const cleanLinksEveryday = pubsub
@@ -256,7 +317,13 @@ export const triggerNotifications = pubsub
   });
 
 const processNotifications = async (app: App) => {
-  if (!process.env.MAILERSEND_API_KEY) return;
+  console.log("Starting processNotifications...");
+  if (!process.env.MAILERSEND_API_KEY) {
+    console.log(
+      "MAILERSEND_API_KEY not found. Exiting processNotifications..."
+    );
+    return;
+  }
   try {
     const database = getDatabase(app);
     const mailersend = new MailerSend({
@@ -264,17 +331,48 @@ const processNotifications = async (app: App) => {
     });
 
     const notificationData = await database.ref("notifications").once("value");
-    if (!notificationData.exists()) return;
+    if (!notificationData.exists()) {
+      console.log("No notifications found. Exiting processNotifications...");
+      return;
+    }
 
     const notification_list = notificationData.val();
 
     for (const index in notification_list) {
       const notification = notification_list[index];
+      console.log(`Processing notification: ${JSON.stringify(notification)}`);
       const notificationType = notification.type as number;
       const congregation = notification.congregation as string;
       const postalCode = notification.postalCode as string;
       const fromUser = notification.fromUser as string;
-      if (!notificationType || !congregation || !postalCode) continue;
+      if (!notificationType || !congregation || !postalCode) {
+        console.log(
+          "Missing notificationType, congregation, or postalCode. Skipping this notification..."
+        );
+        continue;
+      }
+
+      const congregationUsers = await getCongUsers(
+        app,
+        congregation,
+        notificationType === NOTFICATION_TYPE.FEEDBACK
+          ? USER_ROLES.ADMIN
+          : USER_ROLES.CONDUCTOR
+      );
+
+      if (Object.keys(congregationUsers).length === 0) {
+        console.log(
+          `No congregationUsers found for notification: ${JSON.stringify(
+            notification
+          )}. Removing notification...`
+        );
+        await database
+          .ref(
+            `notifications/${congregation}-${postalCode}-${notificationType}`
+          )
+          .remove();
+        continue;
+      }
 
       const addressData = (
         await database
@@ -288,6 +386,11 @@ const processNotifications = async (app: App) => {
       ) as string;
 
       if (!message) {
+        console.log(
+          `No message found for notification: ${JSON.stringify(
+            notification
+          )}. Removing notification...`
+        );
         await database
           .ref(
             `notifications/${congregation}-${postalCode}-${notificationType}`
@@ -295,13 +398,6 @@ const processNotifications = async (app: App) => {
           .remove();
         continue;
       }
-      const congregationUsers = await getCongUsers(
-        app,
-        congregation,
-        notificationType === NOTFICATION_TYPE.FEEDBACK
-          ? USER_ROLES.ADMIN
-          : USER_ROLES.CONDUCTOR
-      );
 
       const territory = await getTerritory(app, congregation, postalCode);
 
@@ -347,7 +443,11 @@ const processNotifications = async (app: App) => {
         .setPersonalization(recipentPersonalization);
 
       try {
+        console.log(
+          `Sending email with parameters: ${JSON.stringify(emailParams)}`
+        );
         await mailersend.email.send(emailParams);
+        console.log("Email sent successfully. Removing notification...");
         await database
           .ref(
             `notifications/${congregation}-${postalCode}-${notificationType}`
@@ -358,6 +458,8 @@ const processNotifications = async (app: App) => {
       }
     }
   } finally {
+    console.log("Deleting app...");
     deleteApp(app);
+    console.log("Finished processNotifications.");
   }
 };
